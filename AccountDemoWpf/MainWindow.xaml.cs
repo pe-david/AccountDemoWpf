563ac74116407bfee6bf1658d2b0181dc91e3c39
David <david.rose@perkinelmer.com>
2019-01-25 10:03:56 -0500

    Minor change. Commiting to stabilize.

--- AccountDemoWpf/MainWindow.xaml.cs
+++ AccountDemoWpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EventStore.Projections.Core.Messages;
 using ReactiveUI;
 
 namespace AccountDemoWpf
